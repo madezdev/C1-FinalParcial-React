20ef6d9102225d7c3b5d9a79f55ca9cc00311055
Martin Hernandez <martinhernandez83@gmail.com>
2023-12-02 08:21:05 -0300

    Update import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/UI/footer/Footer";
 import { NavBar } from "./components/UI/navbar/NavBar";
-import { CardListContainer } from "./page/home";
-import { ContactPage } from "./page/contacto";
+import { CardListContainer } from "./page/home/index";
+import { ContactPage } from "./page/contacto/index";
 import { OdontDetailContainer } from "./components/OdontDetailContainer/OdontDetailContainer";
-import { DestacadoPage } from "./page/destacados";
+import { DestacadoPage } from "./page/destacados/index";
 
 function App() {
   return (
