01e2a9aea336f7cc1367e98d71776ada74dff631
Martin Hernandez <martinhernandez83@gmail.com>
2023-12-02 08:53:04 -0300

    Refactor file structure and component names

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/UI/footer/Footer";
 import { NavBar } from "./components/UI/navbar/NavBar";
-import  Home  from "./page/home/Index";
-import  Contacto  from "./page/contacto/Index";
+import  Home  from "./page/home/Home";
+import  Contacto  from "./page/contacto/Contacto";
 import  {OdontDetailContainer}  from "./components/OdontDetailContainer/OdontDetailContainer";
-import  Destacado  from "./page/destacados/Index";
+import  Destacado  from "./page/destacados/Destacados";
 
 function App() {
   return (
